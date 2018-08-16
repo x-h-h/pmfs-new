ed5461c0bf4c55eec95fcfddc4e4aa0890c19293
x-h-h <236718094@qq.com>
2018-08-16 14:29:20 +0800

    timer_test

--- xip.c
+++ xip.c
@@ -18,7 +18,30 @@
 #include <asm/pgtable.h>
 #include "pmfs.h"
 #include "xip.h"
+#include <linux/module.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
 
+struct timer_list mytimer;
+
+static void myfunc(unsigned long data)
+{
+    printk("%s/n", (char *)data);
+    mod_timer(&mytimer, jiffies + 2*HZ);
+}
+
+static int __init mytimer_init(void)
+{
+    setup_timer(&mytimer, myfunc, (unsigned long)"Hello, world!");
+    mytimer.expires = jiffies + HZ;
+    add_timer(&mytimer);
+    return 0;
+}
+
+static void __exit mytimer_exit(void)
+{
+        del_timer(&mytimer);
+}
 
 int test(void)
 {
