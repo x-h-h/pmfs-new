62d010b69c10111f17d1b3758315d0cb88286e07
x-h-h <236718094@qq.com>
2018-08-16 14:53:34 +0800

    timmer_setup

--- xip.c
+++ xip.c
@@ -18,30 +18,36 @@
 #include <asm/pgtable.h>
 #include "pmfs.h"
 #include "xip.h"
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/timer.h>
-#include <linux/jiffies.h>
+struct timer_list timer;
 
-struct timer_list mytimer;
-
-static void myfunc(unsigned long data)
-{
-    printk("%s/n", (char *)data);
-    mod_timer(&mytimer, jiffies + 2*HZ);
+void timer_handler(unsigned long data) {
+    printk(KERN_INFO"timer pending:%d\n", timer_pending(&timer));
+    mod_timer(&timer, jiffies+msecs_to_jiffies(1000));
+    printk(KERN_INFO"jiffies:%ld, data:%ld\n", jiffies, data);
 }
-
-static int __init mytimer_init(void)
-{
-    timer_setup(&mytimer, myfunc, (unsigned long)"Hello, world!");
-    mytimer.expires = jiffies + HZ;
-    add_timer(&mytimer);
+int timer_init(void) {
+    printk(KERN_INFO"%s jiffies:%ld\n", __func__, jiffies);
+    printk(KERN_INFO"ji:%d,HZ:%d\n", jiffies_to_msecs(250), HZ);
+    init_timer(&timer);
+    timer.data = 45;
+    timer.function = timer_handler;
+    timer.expires = jiffies + HZ;
+    add_timer(&timer);
+    printk(KERN_INFO"timer pending:%d\n", timer_pending(&timer));
     return 0;
 }
 
-static void __exit mytimer_exit(void)
-{
-    del_timer(&mytimer);
+void timer_exit(void) {
+    printk(KERN_INFO"%s jiffies:%ld\n", __func__, jiffies);
+    del_timer(&timer);
 }
+
+module_init(timer_init);
+module_exit(timer_exit);
+MODULE_LICENSE("GPL");
 
 int test(void)
 {
